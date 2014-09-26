ebaa42c81331ea19df3468381e3215e7650455a4
nicholastoddsmith <nicholastoddsmith@gmail.com>
2014-09-26 15:05:31 -0700

    Updated some layout and icons
    Slightly modified the icons and modified the login screen layout

--- Plutus/gen/com/example/plutus/R.java
+++ Plutus/gen/com/example/plutus/R.java
@@ -858,10 +858,13 @@ containing a value of this type.
         public static final int abc_split_action_bar_is_narrow=0x7f060002;
     }
     public static final class color {
-        public static final int abc_search_url_text_holo=0x7f070003;
+        public static final int abc_search_url_text_holo=0x7f070006;
         public static final int abc_search_url_text_normal=0x7f070000;
         public static final int abc_search_url_text_pressed=0x7f070002;
         public static final int abc_search_url_text_selected=0x7f070001;
+        public static final int bkgd_col=0x7f070003;
+        public static final int hint_col=0x7f070005;
+        public static final int txt_col=0x7f070004;
     }
     public static final class dimen {
         /**  Default height of an action bar. 
@@ -1088,14 +1091,15 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
         public static final int action_context_bar=0x7f05001d;
-        public static final int action_help=0x7f05003f;
-        public static final int action_logout=0x7f050040;
+        public static final int action_help=0x7f050040;
+        public static final int action_logout=0x7f050041;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f05003f;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
