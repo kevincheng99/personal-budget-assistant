3722fe371bb4d837a7ce5be9a527f35f687757a4
nicholastoddsmith <nicholastoddsmith@gmail.com>
2014-09-27 13:59:56 -0700

    Modified app UI
    Modified the background, icon, and layout of the UI

--- Plutus/gen/com/example/plutus/R.java
+++ Plutus/gen/com/example/plutus/R.java
@@ -1080,7 +1080,10 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int checker=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
+        public static final int plutuslogo400=0x7f020059;
+        public static final int plutuslogo600=0x7f02005a;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1091,15 +1094,15 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
         public static final int action_context_bar=0x7f05001d;
-        public static final int action_help=0x7f050040;
-        public static final int action_logout=0x7f050041;
+        public static final int action_help=0x7f050041;
+        public static final int action_logout=0x7f050042;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003f;
+        public static final int button1=0x7f050040;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1109,7 +1112,7 @@ containing a value of this type.
         public static final int dropdown=0x7f05000f;
         public static final int edit_password=0x7f05003e;
         public static final int edit_query=0x7f050030;
-        public static final int edit_uername=0x7f05003d;
+        public static final int edit_uername=0x7f05003f;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
@@ -1118,6 +1121,7 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imageView1=0x7f05003d;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1268,6 +1272,7 @@ containing a value of this type.
         public static final int button_update_account_setting=0x7f0a001a;
         public static final int button_view_account_balance=0x7f0a0018;
         public static final int button_view_account_statistic=0x7f0a0019;
+        public static final int content_img=0x7f0a001d;
         public static final int edit_password=0x7f0a0011;
         public static final int edit_username=0x7f0a0010;
         public static final int hello_world=0x7f0a000e;
