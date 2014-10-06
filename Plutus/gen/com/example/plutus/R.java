f06032b559ef2002303d938a5a5c34fb03c8d77c
nicholastoddsmith <nicholastoddsmith@gmail.com>
2014-10-06 13:06:18 -0700

    More Account Balance Edits
    Edit more of the layout for the account balance page

--- Plutus/gen/com/example/plutus/R.java
+++ Plutus/gen/com/example/plutus/R.java
@@ -1082,8 +1082,9 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int checker=0x7f020057;
         public static final int ic_launcher=0x7f020058;
-        public static final int plutuslogo400=0x7f020059;
-        public static final int plutuslogo600=0x7f02005a;
+        public static final int libkfd=0x7f020059;
+        public static final int plutuslogo400=0x7f02005a;
+        public static final int plutuslogo600=0x7f02005b;
     }
     public static final class id {
         public static final int ab_lv=0x7f050048;
