35d2324e629bdf50289600fb07a8dc7e15e391a8
kevincheng99 <kevincheng99@csu.fullerton.edu>
2014-10-10 02:12:44 -0700

    I have integrated the bank database to Plutus. The user information and account threshold in the database can be updated.  For the purpose of Plutus development, I have added a backdoor in the login menu, so we can directly access to the Bank database and perform debug and maintainance. Currently, our database simulation can serve and test three activities: simulation, update and retrieval.
    Once the bank database is simulated, it stayed in the private storage of app
    until the deletion of app. I have added the bank database manager as private
    members in each activity, so each activity can access to the database as in read
    mode. There are two activities having the both read and write mode:
    UpdateAccountSettingActivity and BankDatabaseSimulationActivity. When an
    activity is on pause, we close the connection to the database. When an activity
    is on resume, we open the connection as either read of read-write mode
    accordingly.

--- Plutus/src/com/example/plutus/UpdateAccountSettingActivity.java
+++ Plutus/src/com/example/plutus/UpdateAccountSettingActivity.java
@@ -7,8 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
-public class UpdateAccountSettingActivity extends ActionBarActivity 
-{
+public class UpdateAccountSettingActivity extends ActionBarActivity {
   // Initialize the user id.
   private int userid = -1;
   private EditText et1 = null;
@@ -16,13 +15,15 @@ public class UpdateAccountSettingActivity extends ActionBarActivity
   private EditText et3 = null;
   private EditText et4 = null;
   private Bank bank = new Bank();
-  
+
+  // Initialize the bank database with the bank database manager.
+  private BankDatabaseManager plutusDbManager = new BankDatabaseManager(this);
+
   @Override
-  protected void onCreate(Bundle savedInstanceState) 
-  {
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_update_account_setting);
-    //Find the views
+    // Find the views
     et1 = (EditText) findViewById(R.id.ua_email_tv);
     et2 = (EditText) findViewById(R.id.ua_phone_tv);
     et3 = (EditText) findViewById(R.id.ua_pwd_tv);
@@ -30,24 +31,38 @@ public class UpdateAccountSettingActivity extends ActionBarActivity
 
   }
 
+  @Override
+  public void onDestroy() { // Write any changes back to the bank database
+    bank.WriteChanges(et1.getText().toString(), et2.getText().toString(), et3
+        .getText().toString(), et4.getText().toString());
+    super.onDestroy();
+  }
+
+  // When resume/running/visible to the user, open the database for the read
+  // and write.
+  @Override
+  protected void onResume() {
+    plutusDbManager.openReadWriteMode();
+    super.onResume();
+  }
+
+  // When pause, close any open database.
+  @Override
+  protected void onPause() {
+    plutusDbManager.close();
+    super.onPause();
+  }
 
   @Override
-  public void onDestroy()
-  {	  //Write any changes back to the bank database
-	  bank.WriteChanges(et1.getText().toString(), et2.getText().toString(), et3.getText().toString(), et4.getText().toString());
-	  super.onDestroy();
-  }
-  
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) 
-  { // Inflate the menu; this adds items to the action bar if it is present.
+  public boolean onCreateOptionsMenu(Menu menu) { // Inflate the menu; this adds
+                                                  // items to the action bar if
+                                                  // it is present.
     getMenuInflater().inflate(R.menu.update_account_setting, menu);
     return true;
   }
-  
+
   @Override
-  public boolean onOptionsItemSelected(MenuItem item) 
-  {
+  public boolean onOptionsItemSelected(MenuItem item) {
     // Handle action bar item clicks here. The action bar will
     // automatically handle clicks on the Home/Up button, so long
     // as you specify a parent activity in AndroidManifest.xml.
@@ -55,26 +70,27 @@ public class UpdateAccountSettingActivity extends ActionBarActivity
 
     // Select the help event, logout event or default event.
     switch (id) {
-      case R.id.action_help:
-        // Display a help documentation to describe the activity of update account settings.
+    case R.id.action_help:
+      // Display a help documentation to describe the activity of update account
+      // settings.
 
-        // When successfully handling a menu item, return true.
-        return true;
-      case R.id.action_logout:
-        // Initialize the intent to the login activity.
-        Intent loginIntent = new Intent(this, LoginActivity.class);
+      // When successfully handling a menu item, return true.
+      return true;
+    case R.id.action_logout:
+      // Initialize the intent to the login activity.
+      Intent loginIntent = new Intent(this, LoginActivity.class);
 
-        // Clear the top activities in the task stack and go back to log in activity.
-        loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+      // Clear the top activities in the task stack and go back to log in
+      // activity.
+      loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        // Redirect the user the login activity.
-        startActivity(loginIntent);
+      // Redirect the user the login activity.
+      startActivity(loginIntent);
 
-        // When successfully handling a menu item, return true.
-        return true;
-      default:
-        return super.onOptionsItemSelected(item);
+      // When successfully handling a menu item, return true.
+      return true;
+    default:
+      return super.onOptionsItemSelected(item);
     }
   }
 }
-
--- Plutus/src/com/example/plutus/ViewAccountBalanceActivity.java
+++ Plutus/src/com/example/plutus/ViewAccountBalanceActivity.java
@@ -12,15 +12,15 @@ import android.widget.TextView;
 public class ViewAccountBalanceActivity extends ActionBarActivity {
   // Initialize the user id.
   private int userid = -1;
-  //Amount, spending, and progress bar for savings
+  // Amount, spending, and progress bar for savings
   private TextView acntSpendTv = null;
   private TextView acntAmntTv = null;
   private ProgressBar acntPb = null;
-  //Amount, spending, and progress bar for checking
+  // Amount, spending, and progress bar for checking
   private TextView acntSpendTv2 = null;
   private TextView acntAmntTv2 = null;
   private ProgressBar acntPb2 = null;
-  //Bank object for accessing bank's database
+  // Bank object for accessing bank's database
   private Bank bank = new Bank();
   private Intent vabIntent = null;
   private double userSavBal = 0.0;
@@ -29,48 +29,62 @@ public class ViewAccountBalanceActivity extends ActionBarActivity {
   private double userCheckBal = 0.0;
   private double userCheckThresh = 0.0;
   private double userCheckSpend = 0.0;
-  
-  
+
+  // Initialize the bank database with the bank database manager.
+  private BankDatabaseManager plutusDbManager = new BankDatabaseManager(this);
+
   @Override
-  protected void onCreate(Bundle savedInstanceState) 
-  {
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_view_account_balance);
-    //Get the intent that started the activity
+    // Get the intent that started the activity
     vabIntent = getIntent();
-    //Get the user's id
+    // Get the user's id
     userid = vabIntent.getIntExtra("uid", -1);
-    if(userid == -1)
-    {
-    	//TODO handle -1
+    if (userid == -1) {
+      // TODO handle -1
     }
-    //Get the user's account info
-    //TODO replace these stub functions with the actually account values
+    // Get the user's account info
+    // TODO replace these stub functions with the actually account values
     userSavBal = bank.GetUserBalance(userid);
     userSavThresh = bank.GetUserThreshold(userid);
     userSavSpend = bank.GetUserSpending(userid);
     userCheckBal = bank.GetUserBalance(userid) * 2;
     userCheckThresh = bank.GetUserThreshold(userid) * 2;
     userCheckSpend = bank.GetUserSpending(userid) * 2;
-    //Find views for savings account
+    // Find views for savings account
     acntSpendTv = (TextView) findViewById(R.id.acnt_expns_tv);
     acntAmntTv = (TextView) findViewById(R.id.acnt_amnt_tv);
     acntPb = (ProgressBar) findViewById(R.id.acnt_thresh_pb);
-    //Find views for checking
+    // Find views for checking
     acntSpendTv2 = (TextView) findViewById(R.id.acnt_expns_tv2);
     acntAmntTv2 = (TextView) findViewById(R.id.acnt_amnt_tv2);
     acntPb2 = (ProgressBar) findViewById(R.id.acnt_thresh_pb2);
-    //Set the text views and progress bars appropriately
+    // Set the text views and progress bars appropriately
     acntSpendTv.setText(String.format("%,.2f", userSavSpend));
     acntAmntTv.setText(String.format("%,.2f", userSavBal));
-    //TODO need formula for progress bar
-    acntPb.setProgress(Math.min(100, (int) (userSavBal - userSavThresh))); 
+    // TODO need formula for progress bar
+    acntPb.setProgress(Math.min(100, (int) (userSavBal - userSavThresh)));
     acntSpendTv2.setText(String.format("%,.2f", userCheckSpend));
     acntAmntTv2.setText(String.format("%,.2f", userCheckBal));
-    acntPb2.setProgress(Math.min(100, (int) (userCheckBal - userCheckThresh))); 
+    acntPb2.setProgress(Math.min(100, (int) (userCheckBal - userCheckThresh)));
 
   }
 
+  // When resume/running/visible to the user, open the database for the read
+  // and write.
+  @Override
+  protected void onResume() {
+    plutusDbManager.openReadMode();
+    super.onResume();
+  }
+
+  // When pause, close any open database.
+  @Override
+  protected void onPause() {
+    plutusDbManager.close();
+    super.onPause();
+  }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
@@ -89,25 +103,27 @@ public class ViewAccountBalanceActivity extends ActionBarActivity {
 
     // Select the help event, logout event or default event.
     switch (id) {
-      case R.id.action_help:
-        // Display a help documentation to describe the activity of view account balance.
+    case R.id.action_help:
+      // Display a help documentation to describe the activity of view account
+      // balance.
 
-        // When successfully handling a menu item, return true.
-        return true;
-      case R.id.action_logout:
-        // Initialize the intent to the login activity.
-        Intent loginIntent = new Intent(this, LoginActivity.class);
+      // When successfully handling a menu item, return true.
+      return true;
+    case R.id.action_logout:
+      // Initialize the intent to the login activity.
+      Intent loginIntent = new Intent(this, LoginActivity.class);
 
-        // Clear the top activities in the task stack and go back to log in activity.
-        loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+      // Clear the top activities in the task stack and go back to log in
+      // activity.
+      loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        // Redirect the user the login activity.
-        startActivity(loginIntent);
+      // Redirect the user the login activity.
+      startActivity(loginIntent);
 
-        // When successfully handling a menu item, return true.
-        return true;
-      default:
-        return super.onOptionsItemSelected(item);
+      // When successfully handling a menu item, return true.
+      return true;
+    default:
+      return super.onOptionsItemSelected(item);
     }
 
     // Here are the original Android codes.
@@ -117,19 +133,16 @@ public class ViewAccountBalanceActivity extends ActionBarActivity {
     // return super.onOptionsItemSelected(item);
 
   }
-  
 
-  public void loadSavingTransHistory(View view) 
-  {
+  public void loadSavingTransHistory(View view) {
     // Initialize the intent to user's main activity
     Intent userTrnsIntent = new Intent(this, TransactionHistoryActivity.class);
     userTrnsIntent.putExtra("uid", userid);
     userTrnsIntent.putExtra("acntType", 0);
     startActivity(userTrnsIntent);
   }
-  
-  public void loadCheckTransHistory(View view) 
-  {
+
+  public void loadCheckTransHistory(View view) {
     // Initialize the intent to user's main activity
     Intent userTrnsIntent = new Intent(this, TransactionHistoryActivity.class);
     userTrnsIntent.putExtra("uid", userid);
